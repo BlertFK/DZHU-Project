cd462c053ca5f343687e1a7997e2c7643a59b445
BlertFK <ba70810@ubt-uni.net>
2024-12-11 22:01:13 +0100

    sections added on home and footer

--- script.js
+++ script.js
@@ -1,75 +1,74 @@
-
 // Params
-let mainSliderSelector = '.main-slider',
-  navSliderSelector = '.nav-slider',
+let mainSliderSelector = ".main-slider",
+  navSliderSelector = ".nav-slider",
   interleaveOffset = 0.5;
 
 // Main Slider
 let mainSliderOptions = {
-    loop: true,
-    speed:1000,
-    autoplay: {
-      delay: 5000,
+  loop: true,
+  speed: 1000,
+  autoplay: {
+    delay: 5000,
+  },
+  loopAdditionalSlides: 10,
+  grabCursor: true,
+  watchSlidesProgress: true,
+  navigation: {
+    nextEl: ".swiper-arrow-next",
+    prevEl: ".swiper-arrow-prev",
+  },
+  on: {
+    init: function () {
+      this.autoplay.stop();
     },
-    loopAdditionalSlides: 10,
-    grabCursor: true,
-    watchSlidesProgress: true,
-    navigation: {
-      nextEl: ".swiper-arrow-next",
-      prevEl: ".swiper-arrow-prev"
+    imagesReady: function () {
+      this.el.classList.remove("loading");
+      //this.autoplay.start();
     },
-    on: {
-      init: function(){
-        this.autoplay.stop();
-      },
-      imagesReady: function(){
-        this.el.classList.remove('loading');
-        //this.autoplay.start();
-      },
-      slideChangeTransitionEnd: function(){
-        let swiper = this,
-            captions = swiper.el.querySelectorAll('.caption');
-        for (let i = 0; i < captions.length; ++i) {
-          captions[i].classList.remove('show');
-        }
-        swiper.slides[swiper.activeIndex].querySelector('.caption').classList.add('show');
-      },
-      progress: function(){
-        let swiper = this;
-        for (let i = 0; i < swiper.slides.length; i++) {
-          let slideProgress = swiper.slides[i].progress,
-              innerOffset = swiper.width * interleaveOffset,
-              innerTranslate = slideProgress * innerOffset;
-         
-          swiper.slides[i].querySelector(".slide-bgimg").style.transform =
-            "translateX(" + innerTranslate + "px)";
-        }
-      },
-      touchStart: function() {
-        let swiper = this;
-        for (let i = 0; i < swiper.slides.length; i++) {
-          swiper.slides[i].style.transition = "";
-        }
-      },
-      setTransition: function(speed) {
-        let swiper = this;
-        for (let i = 0; i < swiper.slides.length; i++) {
-          swiper.slides[i].style.transition = speed + "ms";
-          swiper.slides[i].querySelector(".slide-bgimg").style.transition =
-            speed + "ms";
-        }
+    slideChangeTransitionEnd: function () {
+      let swiper = this,
+        captions = swiper.el.querySelectorAll(".caption");
+      for (let i = 0; i < captions.length; ++i) {
+        captions[i].classList.remove("show");
       }
-    }
-  };
+      swiper.slides[swiper.activeIndex]
+        .querySelector(".caption")
+        .classList.add("show");
+    },
+    progress: function () {
+      let swiper = this;
+      for (let i = 0; i < swiper.slides.length; i++) {
+        let slideProgress = swiper.slides[i].progress,
+          innerOffset = swiper.width * interleaveOffset,
+          innerTranslate = slideProgress * innerOffset;
+
+        swiper.slides[i].querySelector(".slide-bgimg").style.transform =
+          "translateX(" + innerTranslate + "px)";
+      }
+    },
+    touchStart: function () {
+      let swiper = this;
+      for (let i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = "";
+      }
+    },
+    setTransition: function (speed) {
+      let swiper = this;
+      for (let i = 0; i < swiper.slides.length; i++) {
+        swiper.slides[i].style.transition = speed + "ms";
+        swiper.slides[i].querySelector(".slide-bgimg").style.transition =
+          speed + "ms";
+      }
+    },
+  },
+};
 let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
 
-
-
-window.onscroll = function() {
-var wScroll = $(window).scrollTop();
-if (wScroll > 0) {
-  $("body").addClass("scroll");
-} else {
-  $("body").removeClass("scroll");
-}
-}
+window.onscroll = function () {
+  var wScroll = $(window).scrollTop();
+  if (wScroll > 0) {
+    $("body").addClass("scroll");
+  } else {
+    $("body").removeClass("scroll");
+  }
+};
